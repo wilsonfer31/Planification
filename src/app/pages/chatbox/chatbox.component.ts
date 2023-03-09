d8a19900674f750a047e3e0577a710a55b4f0daf
WilsonFernandes <wilsonfernandes31470@gmail.com>
2023-03-09 16:42:18 +0100

    chat always scroll down

--- src/app/pages/chatbox/chatbox.component.ts
+++ src/app/pages/chatbox/chatbox.component.ts
@@ -1,51 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Message } from 'src/app/_models/message/MessageDto';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import { environment } from 'src/app/_environement/environment';
 import { AuthService } from 'src/app/_services/authService/auth-service.service';
 import { MessageService } from 'src/app/_services/messageService/message.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
   styleUrls: ['./chatbox.component.css']
 })
-export class ChatboxComponent {
-  
+export class ChatboxComponent implements AfterContentChecked{
+
 
   messages: Message[] = [];
   disabled = true;
   newmessage: string;
-  private stompClient : Stomp.Client | null = null;
+  private stompClient: Stomp.Client | null = null;
   showEmojiPicker = false;
 
   toggleEmojiPicker() {
-    console.log(this.showEmojiPicker);
-        this.showEmojiPicker = !this.showEmojiPicker;
+    this.showEmojiPicker = !this.showEmojiPicker;
   }
 
   addEmoji(event: any) {
-  
     let text = "";
-    if(this.newmessage != null){
-       text = `${this.newmessage}${event.emoji.native}`;
-    }else{
+    if (this.newmessage != null) {
+      text = `${this.newmessage}${event.emoji.native}`;
+    } else {
       text = `${event.emoji.native}`;
     }
+    this.newmessage = text;
+   
+  }
+  constructor(private authService: AuthService, private messageService: MessageService, private cdRef:ChangeDetectorRef) {
+
+  }
+ 
  
 
-    this.newmessage = text;
-    // this.showEmojiPicker = false;
-  }
-  constructor(private authService : AuthService, private messageService : MessageService){
-
-  }
 
   ngOnInit() {
     this.getMessages();
     this.connect();
   }
+
+  ngAfterContentChecked(): void {
+    this.cdRef.detectChanges();
+  }
+
+  
 
   setConnected(connected: boolean) {
     this.disabled = !connected;
@@ -55,63 +61,72 @@ export class ChatboxComponent {
     }
   }
 
-  getMessages(){
+  getMessages() {
     this.messageService.getLastMessages().subscribe({
-      next : (mesageList : Message []) => this.messages = mesageList
+      next: (mesageList: Message[]) => this.messages = mesageList
     }
-    
+
     );
+
+
   }
 
   connect() {
-    const socket = new SockJS(environment.base_api_back  +'/chat');
+
+    const socket = new SockJS(environment.base_api_back + '/chat');
     this.stompClient = Stomp.over(socket);
     this.stompClient.debug = f => f;
 
     const that = this;
-    this.stompClient.connect({}, function (frame : any) {
-      if(that.stompClient != null){
-        that.stompClient.subscribe('/start/initial', (message : any)  => {
-          
+    this.stompClient.connect({}, function (frame: any) {
+      if (that.stompClient != null) {
+        that.stompClient.subscribe('/start/initial', (message: any) => {
+
           that.showMessage(message.body);
-      });
+          that.scrollBottom();
+
+        });
       }
-     
+
     });
   }
 
   sendMessage() {
-   
     let messageDto = {} as Message;
     messageDto.message = this.newmessage;
     messageDto.sender = this.authService.currentUserValue.email!;
 
-  
+
 
     this.stompClient?.send(
       '/current/resume',
       {},
       JSON.stringify(messageDto)
     );
-   
+
     this.newmessage = "";
   }
 
-  showMessage(message : string) {
+  showMessage(message: string) {
 
     let data = JSON.parse(message);
     this.messages.push(data);
-   
+
 
   }
 
-  didISend(message : Message){
+  didISend(message: Message) {
 
-    if(message.sender == this.authService.currentUserValue.email){
-    
+    if (message.sender == this.authService.currentUserValue.email) {
+
       return true;
     }
     return false;
+  }
+
+  scrollBottom() {
+    var objDiv = document.getElementById("chat");
+    objDiv!.scrollTop = objDiv!.scrollHeight;
   }
 }
 
